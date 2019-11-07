dd75863695b1cdb57380bac4c57733b09500fe22
georgegoldman <georgegoldman2014@gmail.com>
2019-11-07 14:37:37 +0100

    change made to the init and run files

--- application/__init__.py
+++ application/__init__.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from flask_admin import Admin
 from flask_fontawesome import FontAwesome
 
 app = Flask(__name__)
