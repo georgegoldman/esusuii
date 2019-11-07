ce0072627088a0c0f47ef2efeb545fb4da04e6fc
georgegoldman <georgegoldman2014@gmail.com>
2019-11-07 14:51:03 +0100

    update __init__ & run

--- application/__init__.py
+++ application/__init__.py
@@ -2,8 +2,6 @@ import os
 from flask import Flask, Markup
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
 from flask_fontawesome import FontAwesome
 
 app = Flask(__name__)
@@ -16,11 +14,7 @@ app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
 
 
 db = SQLAlchemy(app)
-migrate =  Migrate(app, db)
 fa = FontAwesome(app)
-
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
 
 
 from application import models
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from application import  manager, app
+from application import app
 
 
 
