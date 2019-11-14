eb8c8e9e2757cc3b9efd116eb193a58daf2c73ee
georgegoldman <georgegoldman2014@gmail.com>
2019-11-14 10:20:08 +0100

    updt

--- application/static/js/pop-up.js
+++ application/static/js/pop-up.js
@@ -3,7 +3,7 @@ confirm('testin the javascript file connection '  );
 })
 
 function joinGroup(group_id) {
-  fetch(`https://esusu-app.herokuapp.com/join_group?group_id=${group_id}`)
+  fetch(`http://esusu-app.herokuapp.com/join_group?group_id=${group_id}`)
     .then(res => {
       return res.text()
     ;
@@ -22,7 +22,7 @@ function leaveGroup(group_id, user_id, member_id) {
     y = confirm('Click ok to confirm leave');
     y;
     if (y == true){
-      fetch(`https://esusu-app.herokuapp.com/leave_group?group_id=${group_id}`)
+      fetch(`http://esusu-app.herokuapp.com/leave_group?group_id=${group_id}`)
 
         .then(res => {
           return res.text()
