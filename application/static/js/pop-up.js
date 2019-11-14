97b5363cac69952b068a1c10d482aa16e99efff7
georgegoldman <georgegoldman2014@gmail.com>
2019-11-14 15:02:34 +0100

    updt

--- application/static/js/pop-up.js
+++ application/static/js/pop-up.js
@@ -20,7 +20,7 @@ function leaveGroup(group_id, user_id, member_id) {
     y = confirm('Click ok to confirm leave');
     y;
     if (y == true){
-      fetch(`http://esusu-app.herokuapp.com/leave_group?group_id=${group_id}`)
+      fetch(`http://esusu-app.herokuapp.com/join_group?group_id=${group_id}`)
 
         .then(res => {
           return res.text()
